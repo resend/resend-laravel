61a7ddb69e98a9814fb627c48d20aee71ac5fdd0
Jayan Ratna <jayanratna@gmail.com>
2023-05-03 09:23:30 +1200

    add Facade support for updated PHP SDK

--- tests/Facades/Resend.php
+++ tests/Facades/Resend.php
@@ -2,6 +2,9 @@
 
 use Resend\Client;
 use Resend\Laravel\Facades\Resend;
+use Resend\Service\ApiKey;
+use Resend\Service\Domain;
+use Resend\Service\Email;
 
 it('resolves Resend client', function () {
     $app = app();
@@ -11,4 +14,16 @@ it('resolves Resend client', function () {
     ]);
 
     expect(Resend::getFacadeRoot())->toBeInstanceOf(Client::class);
+});
+
+it('can get an API service', function () {
+    $app = app();
+
+    $app->get('config')->set('resend', [
+        'api_key' => 'test',
+    ]);
+
+    expect(Resend::apiKeys())->toBeInstanceOf(ApiKey::class)
+        ->and(Resend::domains())->toBeInstanceOf(Domain::class)
+        ->and(Resend::emails())->toBeInstanceOf(Email::class);
 });
