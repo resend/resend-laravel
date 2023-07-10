9d944b6930c09fc9a3891d7356c75657e57b33ef
Jayan Ratna <jayanratna@gmail.com>
2023-07-10 16:06:42 +1200

    add tests for multiple recipients

--- tests/Transport/ResendTransportFactory.php
+++ tests/Transport/ResendTransportFactory.php
@@ -33,7 +33,7 @@ test('constructor', function () {
     expect($this->transporter)->toBeInstanceOf(ResendTransportFactory::class);
 });
 
-test('send', function () {
+test('can send', function () {
     $email = (new SymfonyEmail())
         ->from('from@example.com')
         ->to(new Address('to@example.com', 'Acme'))
@@ -60,6 +60,33 @@ test('send', function () {
                 $arg['cc'] === ['cc@example.com'] &&
                 $arg['bcc'] === ['bcc@example.com'] &&
                 $arg['reply_to'] === ['reply-to@example.com'];
+        }))
+        ->andReturn($apiResponse);
+
+    $this->transporter->send($email);
+});
+
+test('can send to multiple recipients', function () {
+    $email = (new SymfonyEmail())
+        ->from('from@example.com')
+        ->to(new Address('to@example.com', 'Acme'), new Address('sales@example.com', 'Acme Sales'))
+        ->subject('Test Subject')
+        ->text('Test plain text body')
+        ->html('<p>Test HTML body</p>');
+
+    $apiResponse = new Email([
+        'id' => '49a3999c-0ce1-4ea6-ab68-afcd6dc2e794',
+        'from' => 'from@example.com',
+        'to' => 'to@example.com',
+        'created_at' => '2022-07-25T00:28:32.493138+00:00',
+    ]);
+
+    $this->client->emails
+        ->shouldReceive('send')
+        ->once()
+        ->with(Mockery::on(function ($arg) {
+            return $arg['from'] === 'from@example.com' &&
+                $arg['to'] === ['"Acme" <to@example.com>', '"Acme Sales" <sales@example.com>'];
         }))
         ->andReturn($apiResponse);
 
