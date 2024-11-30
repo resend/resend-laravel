9f31f8eac8fe2a518ad55acdc2ffb9d074300152
Shachar MW <53349272+cheddZy@users.noreply.github.com>
2024-11-30 16:42:17 +0200

    Added to Resend.php Facade: Contact, Audience and Batch methods

--- src/Facades/Resend.php
+++ src/Facades/Resend.php
@@ -4,6 +4,9 @@ namespace Resend\Laravel\Facades;
 
 use Illuminate\Support\Facades\Facade;
 use Resend\Service\ApiKey;
+use Resend\Service\Audience;
+use Resend\Service\Batch;
+use Resend\Service\Contact;
 use Resend\Service\Domain;
 use Resend\Service\Email;
 
@@ -22,6 +25,20 @@ class Resend extends Facade
     public static function emails(): Email
     {
         return static::getFacadeRoot()->emails;
+    }
+
+    public static function contacts(): Contact
+    {
+        return static::getFacadeRoot()->contacts;
+    }
+
+    public static function audiences(): Audience
+    {
+        return static::getFacadeRoot()->audiences;
+    }
+    public static function batch(): Batch
+    {
+        return static::getFacadeRoot()->batch;
     }
 
     /**
