de4b144784d84cb743166e6306c3f45697572493
Shachar MW <53349272+cheddZy@users.noreply.github.com>
2026-02-12 22:41:26 +0200

    Added to Resend.php Facade: Contact, Audience and Batch methods (#70)
    Co-authored-by: Jayan Ratna <jayanratna@gmail.com>

--- src/Facades/Resend.php
+++ src/Facades/Resend.php
@@ -3,27 +3,27 @@
 namespace Resend\Laravel\Facades;
 
 use Illuminate\Support\Facades\Facade;
-use Resend\Service\ApiKey;
-use Resend\Service\Domain;
-use Resend\Service\Email;
 
+/**
+ * Provides Resend integration for Laravel and Symfony Mailer.
+ *
+ * @method static \Resend\Service\ApiKey apiKeys()
+ * @method static \Resend\Service\Audience audiences()
+ * @method static \Resend\Service\Batch batch()
+ * @method static \Resend\Service\Broadcast broadcasts()
+ * @method static \Resend\Service\Contact contacts()
+ * @method static \Resend\Service\ContactProperty contactProperties()
+ * @method static \Resend\Service\Domain domains()
+ * @method static \Resend\Service\Email emails()
+ * @method static \Resend\Service\Segment segments()
+ * @method static \Resend\Service\Template templates()
+ * @method static \Resend\Service\Topic topics()
+ * @method static \Resend\Service\Webhook webhooks()
+ *
+ * @see \Resend\Client
+ */
 class Resend extends Facade
 {
-    public static function apiKeys(): ApiKey
-    {
-        return static::getFacadeRoot()->apiKeys;
-    }
-
-    public static function domains(): Domain
-    {
-        return static::getFacadeRoot()->domains;
-    }
-
-    public static function emails(): Email
-    {
-        return static::getFacadeRoot()->emails;
-    }
-
     /**
      * Get the registered name of the component.
      */
--- tests/Facades/Resend.php
+++ tests/Facades/Resend.php
@@ -3,8 +3,17 @@
 use Resend\Client;
 use Resend\Laravel\Facades\Resend;
 use Resend\Service\ApiKey;
+use Resend\Service\Audience;
+use Resend\Service\Batch;
+use Resend\Service\Broadcast;
+use Resend\Service\Contact;
+use Resend\Service\ContactProperty;
 use Resend\Service\Domain;
 use Resend\Service\Email;
+use Resend\Service\Segment;
+use Resend\Service\Template;
+use Resend\Service\Topic;
+use Resend\Service\Webhook;
 
 it('resolves Resend client', function () {
     $app = app();
@@ -24,6 +33,15 @@ it('can get an API service', function () {
     ]);
 
     expect(Resend::apiKeys())->toBeInstanceOf(ApiKey::class)
+        ->and(Resend::audiences())->toBeInstanceOf(Audience::class)
+        ->and(Resend::batch())->toBeInstanceOf(Batch::class)
+        ->and(Resend::broadcasts())->toBeInstanceOf(Broadcast::class)
+        ->and(Resend::contacts())->toBeInstanceOf(Contact::class)
+        ->and(Resend::contactProperties())->toBeInstanceOf(ContactProperty::class)
         ->and(Resend::domains())->toBeInstanceOf(Domain::class)
-        ->and(Resend::emails())->toBeInstanceOf(Email::class);
+        ->and(Resend::emails())->toBeInstanceOf(Email::class)
+        ->and(Resend::segments())->toBeInstanceOf(Segment::class)
+        ->and(Resend::templates())->toBeInstanceOf(Template::class)
+        ->and(Resend::topics())->toBeInstanceOf(Topic::class)
+        ->and(Resend::webhooks())->toBeInstanceOf(Webhook::class);
 });
